48ecb680d5d481aebc6390ce4a07026aff4e6b38
Benedikt Strahm <benedikt.strahm@ilek.uni-stuttgart.de>
2022-11-10 19:07:21 +0100

    add datum planes in assembly

--- src/abq_assembly.py
+++ src/abq_assembly.py
@@ -44,3 +44,36 @@ def create_boolean_merge_assembly(model, instanceNames, name):
     i = a.InstanceFromBooleanMerge(name=name, instances=(
         instanceObjs), keepIntersections=ON, originalInstances=SUPPRESS, domain=GEOMETRY)
     a.makeIndependent(instances=(i, ))
+
+
+def create_datum_plane_by_point_and_normal(model, part, point, normal):
+    if not normal[0] == 0:
+        principalPlane = YZPLANE
+        offset = point[0]
+    elif not normal[1] == 0:
+        principalPlane = XZPLANE
+        offset = point[1]
+    elif not normal[2] == 0:
+        principalPlane = XYPLANE
+        offset = point[2]
+
+    a = model.rootAssembly
+    plane = a.DatumPlaneByPrincipalPlane(
+        principalPlane=principalPlane, offset=offset)
+    myID = plane.id
+    return myID
+
+
+def create_datum_plane_by_principal(model, part, principalPlane, offset):
+    a = model.rootAssembly
+    plane = a.DatumPlaneByPrincipalPlane(
+        principalPlane=principalPlane, offset=offset)
+    myID = plane.id
+    return myID
+
+
+def create_partition_by_datum_plane(model, instance, id_plane):
+    a = model.rootAssembly
+    c = a.instances[instance].cells[:]
+    d = a.datums
+    a.PartitionCellByDatumPlane(datumPlane=d[id_plane], cells=c, )
