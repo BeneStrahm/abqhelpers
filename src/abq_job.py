0f00e2a8e93aa4dd71df1eee64838a1c66978159
Benedikt Strahm <benedikt.strahm@ilek.uni-stuttgart.de>
2022-11-10 19:07:29 +0100

    initial commit

---
+++ src/abq_job.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# Description:  Helper functions for ABAQUS Property
+# Author:       benedikt.strahm@ilek.uni-stuttgart.de
+# Created:      03.11.2022
+# Execution:    Import functions / collections (from pyLek.helpers import util)
+# Status:       In Progress
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Sources
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Libraries
+# ------------------------------------------------------------------------------
+
+from abaqus import *
+from abaqusConstants import *
+
+# ------------------------------------------------------------------------------
+# Classes
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+
+
+def create_job(model, job_name, cpu):
+    a = model.rootAssembly
+    a.regenerate()
+    mdb.Job(name=job_name, model=model.name, description='', 
+            type=ANALYSIS, atTime=None, waitMinutes=0, waitHours=0, queue=None, 
+            memory=90, memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True, 
+            explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
+            modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='', 
+            scratch='', resultsFormat=ODB, multiprocessingMode=DEFAULT, numCpus=cpu,
+            numDomains=cpu, numGPUs=0)
+
+# ------------------------------------------------------------------------------
+
+
+def submit_job(job_name):
+    mdb.jobs[job_name].submit(consistencyChecking=OFF)
+
+
+# mdb.Job(name='Job-1', model='Concrete_beam_4_P_B', description='hihi',
+#         scratch='', resultsFormat=ODB, multiprocessingMode=DEFAULT, numCpus=4,
+#         numDomains=4, numGPUs=0)
